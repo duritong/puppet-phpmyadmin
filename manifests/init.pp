2b51dd030a2e7f643622c9dbe43d423c0d204571
andreas <andreas@immerda.ch>
2008-02-29 17:49:30 +0000

    * php.ini default file * webapp-config module (only gentoo) * phpmyadmin modul (erster teil)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,34 @@
-# modules/skeleton/manifests/init.pp - manage skeleton stuff
+# modules/phpmyadmin/manifests/init.pp - manage phpmyadmin stuff
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-# modules_dir { "skeleton": }
+# modules_dir { "phpmyadmin": }
 
-class skeleton {
+class phpmyadmin {
 
+    case $operatingsystem {
+        gentoo: { include webapp-config }
+    }
+    
+    package { 'phpmyadmin':
+        ensure => present,
+        category => $operatingsystem ? {
+            gentoo => 'dev-db',
+            default => '',
+        }
+    }
+
+    # config files
+#    file{
+#        "/var/www/localhost/htdocs/phpmyadmin/":
+#            source => [
+#                "puppet://$server/dist/php/apache2_php5_php.ini/${fqdn}/php.ini",
+#                "puppet://$server/php/apache2_php5_php.ini/${fqdn}/php.ini",
+#                "puppet://$server/php/apache2_php5_php.ini/php.ini"
+#            ],
+#            owner => root,
+#            group => 0,
+#            mode => 0644,
+#            require => Package[phpmyadmin],
+#    }
 }
