f60fbaf6b5ffe2f99076a973761339606a3e70f7
mh <mh@immerda.ch>
2016-09-16 21:40:50 +0200

    make sure we return nothin when nothin is installed

--- lib/facter/phpmyadmin_version.rb
+++ lib/facter/phpmyadmin_version.rb
@@ -2,6 +2,7 @@
 Facter.add('phpmyadmin_version') do
   confine :osfamily => ['RedHat']
   setcode do
-    Facter::Util::Resolution.exec('rpm -q --queryformat \'%{VERSION}\' phpMyAdmin')
+    s = Facter::Util::Resolution.exec('rpm -q --queryformat \'%{VERSION}\' phpMyAdmin')
+    s == 'package phpMyAdmin is not installed' ? nil : s
   end
 end
